bf119ca710335e2039ea942d5ce1d5635d6e1f6a
Anders Jakobsen <andersjkbsn@gmail.com>
2019-05-04 15:02:04 +0200

    Using new api

--- client/src/app/model/product.model.ts
+++ client/src/app/model/product.model.ts
@@ -5,4 +5,5 @@ export class Product {
   price: number;
   category: string;
   stock: number;
+  collection: string;
 }
--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBase: 'http://kiosk.aaulan.dk:8000/kiosk/api/v1.0',
+  apiBase: 'http://kiosk.aaulan.dk:8000',
   key: 'test'
 };
